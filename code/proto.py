8c4249e5ac405b64d8597ae6697e02d3e8bb45ed
raste <pd>
2021-12-06 14:05:40 +0000

    test filtering

--- code/proto.py
+++ code/proto.py
@@ -1,6 +1,8 @@
 import sys
 import os
 import cv2
+import numpy as np
+import time
 
 #Checking that this script is running on python2.7
 print("Using python"+str(sys.version_info.major)+"."+str(sys.version_info.minor)) 
@@ -10,7 +12,12 @@ assert sys.version_info.major == 2 and sys.version_info.minor == 7
 #in :  image_raw = image prise par le robot [ici RGB mais surement GRAY]
 #in :  image_filtered = image filtree [NOIR et BLANC]
 def func_preprocess(image_raw):
-    image_filtered = None
+    image_filtered = cv2.cvtColor(image_raw, cv2.COLOR_BGR2GRAY)
+    image_filtered = cv2.equalizeHist(image_filtered)
+    image_filtered = cv2.medianBlur(image_filtered, 25)
+    kernel0 = np.ones((5, 5), np.uint8)
+    image_filtered = cv2.dilate(image_filtered, kernel0)
+    image_filtered = cv2.erode(image_filtered, kernel0)
     return image_filtered
 
 #in :  image_filtered = image filtree [NOIR et BLANC]
@@ -41,13 +48,16 @@ if __name__ == "__main__":
 
     #for all the images we have
     for image_path in image_list:
+        
         #0 LOADING OPENCV IMAGE
 
         image_raw = cv2.imread(image_path)
-        cv2.imshow('image',image_raw)
-        
+        cv2.imshow('image_raw',image_raw)
+        start_time = time.time()
         #1 PREPROCESSING
         image_filtered=func_preprocess(image_raw)
+        cv2.imshow('image_filtered',image_filtered)
+
         #2 EDGE FILTERING 
         #   Je vais rapidement fournir une V0 de la fonction de filtrage 
         #   en attendant tu peux essayer sur des images non filtrees  
@@ -58,7 +68,7 @@ if __name__ == "__main__":
         #   En attendant que guilhem vous sorte une image des contours
         #   vous devrez bosser sur une image paint  
         is_defective,hole_type  = func_edge_analysis(image_edges,8)
-
+        print("--- TIME : %s seconds ---" % (time.time() - start_time))
 
         print("(press a key to process next image)")
         cv2.waitKey(0)
