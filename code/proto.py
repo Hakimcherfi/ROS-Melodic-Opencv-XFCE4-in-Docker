623d8f85b03a2841bc808300eb5f29e519174bc5
Your Name <you@example.com>
2021-12-07 15:46:38 +0000

    ajout du G

--- code/proto.py
+++ code/proto.py
@@ -3,6 +3,11 @@ import os
 import cv2
 import numpy as np
 import time
+from skimage import data, img_as_float
+from skimage.segmentation import (morphological_chan_vese,
+                                  morphological_geodesic_active_contour,
+                                  inverse_gaussian_gradient,
+                                  checkerboard_level_set)
 
 #Checking that this script is running on python2.7
 print("Using python"+str(sys.version_info.major)+"."+str(sys.version_info.minor)) 
@@ -12,32 +17,33 @@ assert sys.version_info.major == 2 and sys.version_info.minor == 7
 #in :  image_raw = image prise par le robot [ici RGB mais surement GRAY]
 #in :  image_filtered = image filtree [NOIR et BLANC]
 def func_preprocess(image_raw):
+
     image_filtered = cv2.cvtColor(image_raw, cv2.COLOR_BGR2GRAY)
     image_filtered = cv2.medianBlur(image_filtered, 25)
     cv2.imshow('test',image_filtered)
+    image_filtered = cv2.adaptiveThreshold(image_filtered,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,41,10)
 
-    #image_filtered = cv2.equalizeHist(image_filtered)
-    #ret, image_filtered = cv2.threshold(image_filtered, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY)
-    image_filtered = cv2.adaptiveThreshold(image_filtered,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,21,8)
-    image_filtered = cv2.medianBlur(image_filtered,5)
-
-    image_filtered = cv2.medianBlur(image_filtered,5)
-    image_filtered = cv2.medianBlur(image_filtered,5)
-    image_filtered = cv2.medianBlur(image_filtered,5)
-
-    #kernel0 = np.ones((2, 2), np.uint8)
-    #image_filtered = cv2.dilate(image_filtered, kernel0)
-    #kernel0 = np.ones((3, 3), np.uint8)
-    #image_filtered = cv2.erode(image_filtered, kernel0)
-
-
-    return image_filtered
+    return image_filtered 
 
 #in :  image_filtered = image filtree [NOIR et BLANC]
 #out : image_edges    = image du coutour du trou [NOIR et BLANC]
 def func_edge_detection(image_filtered):
-    image_edges = None
-    return image_edges
+
+    # Sobel Edge Detection    
+    # sobelxy = cv2.Sobel(src=image_filtered, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=5) # Combined X and Y Sobel Edge Detection
+    # # Display Sobel Edge Detection Images    
+    # cv2.imshow('Sobel Edge Detection', sobelxy)
+    # cv2.waitKey(0)
+
+    # Canny Edge Detection
+    edges = cv2.Canny(image_filtered,100,300,3) # Canny Edge Detection
+    # Display Canny Edge Detection Image
+    cv2.imshow('Canny Edge Detection', edges)
+
+    #Laplacian Edge Detection
+    # laplacian = cv2.Laplacian(image_filtered,cv2.CV_64F)
+    # cv2.imshow('Laplacian Edge Detection',laplacian)
+    #cv2.waitKey(0)
 
 #int : image_edges               = image du coutour du trou [NOIR et BLANC]
 #      expected_hole_diameter    = diametre suppose du trou [FLOAT] (data optenue par autre pole que nous)
