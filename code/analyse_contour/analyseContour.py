0de889759d6e45c9f327c7003db5abf702cb0258
Hakimcherfi <hakimcherfi31@gmail.com>
2021-12-08 16:20:56 +0100

    bugfix contours, limited to one form

--- code/analyse_contour/analyseContour.py
+++ code/analyse_contour/analyseContour.py
@@ -7,76 +7,69 @@ def contour(image):
     grey = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY) #conversion greyscale
     ret,im_thresh = cv2.threshold(grey,127,255,cv2.THRESH_BINARY)
     contours,hierarchy = cv2.findContours(im_thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
-    if contours is None:
-        return None
-    nb_contours = len(contours)
-    x,y = 0,0
-    aire = 0
-    for contour in contours:
-        moments = cv2.moments(contour)
-        x += moments['m10']/moments['m00']
-        y += moments['m01']/moments['m00']
-        aire += moments['m00']
-    x,y,aire = np.round(x/nb_contours).astype("int"),np.round(y/nb_contours).astype("int"),np.round(aire/nb_contours).astype("int")
+    assert (len(contours)==1),"plusieurs contours detectes"
+    contours = contours[0]
+    moments = cv2.moments(contours)
+    x = moments['m10']/moments['m00']
+    y = moments['m01']/moments['m00']
+    aire = moments['m00']
+    x,y,aire = np.round(x).astype("int"),np.round(y).astype("int"),np.round(aire).astype("int")
     return contours,x,y,aire
 
 def cercle(image):
     grey = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY) #conversion greyscale
     method = cv2.HOUGH_GRADIENT
     dp = 1 #0.5 : step 2 fois plus grand que resolution image. 2 : step 2 fois plus petit que resolution image
-    minDist = 100000 #distance minimale entre cercles detectes (detecter cercle unique si assez grand, voir aucun si trop grand)
+    minDist = 100000 #distance minimale entre cercles detectes (detecter cercle unique si assez grand, voir aucun si trop grand) mais le premier est conserve...
     param1 = 1#	seuil superieur donne a canny (seuil inf= moitie seuil sup)
     param2 = 1# seuil inferieur utilise pour mecanisme de vote (si bas, des faux cercles sont detectes)
     minRadius = 0#seuil inf des cercles recherches
     maxRadius = 0#seuil sup des cercles recherches (si <=0 utilise +gde dimension de l'image, si <0, retourne centres sans donner de rayons)
     circles = cv2.HoughCircles(grey,method,dp,minDist,param2 = param2) #liste de tuples contenant coordonnees cercles et votes
-    return circles
+    return circles[:1,:1,:].reshape(1,3)
 
-def caracterization(image,diametre,affichage=False):
+def caracterization(image,rayon,affichage=False):
     """
     fonction caracterization
     in :
     -image (numpy array) binaire du contour unique et ferme du trou (contour a 255, fond a 0)
-    -diametre suppose du trou dans image
+    -rayon suppose du trou dans image
     -affichage : booleen pour creer fenetre affichage des differentes etapes
     out :
     - isdefective : booleen
     - defect : string contenant une forme/defaut reconnue
     """
     isdefective = False
-    defect = "No defect"
+    defect = ""
     circle = cercle(image)
     if circle is None:
         print("no circle found.")
         return True,"no circle found."
-    
     contours,xbar,ybar,aire = contour(image)
     if contours is None:
         print("issue with contours detection.")
         return True,"issue with contours detection."
-
     #caracterisation :
 
     #entre hough et les contours (la forme est-elle un cercle ?)
-    if (circle[:1,:1,2:]*0.9<=(aire/np.pi)**0.5<=circle[:1,:1,2:]*1.1):
+    if (np.pi*(circle[:,2:])**2)*0.9<=aire<=np.pi*((circle[:,2:])**2)*1.1:
         pass
     else:
         isdefective=True
-        defect = "mismatch cercle/contours ({}/{})".format(float(circle[:1,:1,2:]),(aire/np.pi)**0.5)
+        defect = "area mismatch cercle/contours ({}/{})\n".format(float(np.pi*((circle[:,2:])**2)),aire)
     
-    if (circle[:1,:1,2:]*0.9<=diametre/2<=circle[:1,:1,2:]*0.9):
+    if (circle[:,2:]*0.9<=rayon<=circle[:,2:]*1.1):
         pass
     else:
         isdefective=True
-        defect += " and radius mismatch parameter/circle ({}/{})".format(diametre/2,float(circle[:1,:1,2:]))
+        defect += "radius mismatch function parameter/cercle ({}/{})\n".format(rayon,float(circle[:,2:]))
     
     if affichage:
         cv2.imshow('original image',image)
         cv2.waitKey(0)
         output = image.copy()
-        circle = np.round(circle[0, :]).astype("int") #pixels : valeurs entieres
-        for (x, y, r) in circle:
-            cv2.circle(output,(x,y),r,(0,0,255),1) #dessine cercle
+        circle = np.round(circle[:, :]).astype("int") #pixels : valeurs entieres
+        cv2.circle(output,(circle[:,:1],circle[:,1:2]),circle[:,2:],(0,0,255),1) #dessine cercle
         cv2.imshow('transfo hough',output)
         cv2.waitKey(0)
         output = np.zeros(image.shape)
@@ -87,7 +80,7 @@ def caracterization(image,diametre,affichage=False):
     return isdefective, defect
 
 if __name__ == "__main__":
-    image = cv2.imread('closed_form.jpg')
-    defect,resultat = caracterization(image,50,True)
+    image = cv2.imread('ovale.jpg')
+    defect,resultat = caracterization(image,100,True)
     print(defect)
     print(resultat)
