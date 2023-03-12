9bfe1afece17c72b469b935e7802ab4ac1550ae3
hakimcherfi <hakimcherfi31@gmail.com>
2023-03-12 10:30:16 +0100

    Bugfix launch.sh

--- launch.sh
+++ launch.sh
@@ -1,5 +1,5 @@
 tag=ros
 volume=volume_map
-mkdir $(volume)
+mkdir $volume
 docker build . --tag $tag
-docker run --rm -it -v $(pwd)/$(volume):/root/Desktop/$(volume) -p 6080:6080 $tag
+docker run --rm -it -v $(pwd)/$volume:/root/Desktop/$volume -p 6080:6080 $tag
